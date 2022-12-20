3af4f0ebbdab9c668587c833a8f9cd29b13564f3
Alisson Lins <alissonlinsbispo@gmail.com>
2022-12-20 18:16:44 -0300

    Finalização do projeto

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+let api_key = "6da7665eb1b4060ff157eae566f09b61"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
